4ff2f7b404fac53f2bb261f820d495975cf56dad
Salvador Martinez <cartoonrocket9@gmail.com>
2021-08-23 15:08:49 +0200

    DOM & array iterators compiled files

--- js-practice/array-iterators/sandbox.js
+++ js-practice/array-iterators/sandbox.js
@@ -1,34 +1,9 @@
-const pascalCaseVariableName = "IWantToBeSnakeCase";
 
-/* const convertPascalCaseToSnakeCase = () => {
-  const snake_case_string = "";
-  for (let index = 0; index < pascalCaseVariableName.length; index++ ) {
-    if (pascalCaseVariableName[index] === pascalCaseVariableName[index].toUpperCase()) {
-      snake_case_string = snake_case_string + pascalCaseVariableName[index].toLowerCase() + "_";
-    } else {
-      snake_case_string = snake_case_string + pascalCaseVariableName[index];
-    }
-  }
-
-  return snake_case_string;
-}; */
-
-/* let aaa = pascalCaseVariableName.split(/(?=[A-Z])/);
-console.log(aaa); */
-
-/* var newString = pascalCaseVariableName.replace(/(?=[A-Z])/,toLowerCase()) */
-
-/* console.log(newString); */
-
-function styleHyphenFormat(propertyName) {
-  function upperToHyphenLower(match) {
-    return "_" + match.toLowerCase();
-  }
-
-  let newString = propertyName.replace(/[A-Z]/g, upperToHyphenLower);
-  newString = newString.substring(1);
-
-  return newString;
+let customer = {
+  fullName: 'max tomy'
 }
+let nameArr = customer.fullName.split(" ");
+console.log();
 
-console.log(styleHyphenFormat(pascalCaseVariableName));
+let firstName = nameArr[0];
+console.log(firstName);
